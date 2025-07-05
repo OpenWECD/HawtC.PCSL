acfc617e2bd470e1af342064e08884e099d76d60
赵子祯 <1821216780@qq.com>
2025-07-05 18:11:16 +0800

    更新

---
+++ README.md
@@ -0,0 +1,80 @@
+## HawtC.PCS
+
+Beta V2.0.014及其之后的HawtC以及PcsL版本支持FEM方法计算截面特性以及翼型网格自动化算法。
+
+需要注意的是：PCS的算法分为经典层压板理论和有限元方法，层压板的代码完全是PreComp的c#版本。但有限元方法是我们自己开发的代码。为了方便用户，我们参考了PreComp的输入文件
+
+并对其进行了二次开发。下面我们给出PCSL有限元方法计算截面矩阵的与商业软件VABS以及BECAS、FAROB、CROSTAB的数值验证对比。
+
+相关案例文件请前往 https://github.com/OpenWECD/HawtC.Public/tree/main/demo/PCSL 下载
+
+## 0、How To Use?
+
+HawtC 是部分开源且免费使用的计算软件，需要您申请免费的许可证来使用，我们提供了自动化的许可证管理系统，您只需要登录我们的
+
+网站：http://www.hawtc.cn 或 http://www.openwecd.fun/ 获取支持！
+
+![1751571229053](image/README/1751571229053.png)
+
+## 🧑‍💻 开发环境
+
+<div align="center">
+  <img src="https://skillicons.dev/icons?i=cs,python,cpp,dotnet,visualstudio,vscode,windows" />
+</div>
+
+## 验证案例
+
+#### 案例1 均匀矩形截面验证
+
+##### 1、截面模型网格
+
+![1751612560460](image/README/1751612560460.png)
+
+##### 2、计算结果
+
+![1751648002857](image/README/1751648002857.png)
+
+#### 案例2 非均匀矩形截面验证
+
+##### 1、截面模型网格
+
+![1751613198604](image/README/1751613198604.png)
+
+##### 2、计算结果
+
+![1751647968980](image/README/1751647968980.png)
+
+#### 案例3 圆环结构
+
+##### 1、截面模型网格
+
+![1751646962310](image/README/1751646962310.png)
+
+##### 2、计算结果
+
+![1751648043462](image/README/1751648043462.png)
+
+![1751648124507](image/README/1751648124507.png)
+
+
+
+## 源代码下载
+
+请访问[www.HawtC.cn](http://www.openwecd.fun/)
+
+## 交流论坛
+
+交流论坛 http://www.openwecd.fun:22304/
+
+## 开发者
+
+<divalign="center">
+
+## 开发者
+
+<div align="center">
+  <h2>✨ 开发者</h2>
+  <a href="https://github.com/OpenWECD/HawtC.Public/graphs/contributors">
+    <img src="https://contrib.rocks/image?repo=OpenWECD/HawtC.Public&max=100&columns=8&size=60" />
+  </a>
+</div>
---
+++ src/HawtC2.ProComp.Main.cs
@@ -0,0 +1,140 @@
+﻿//**********************************************************************************************************************************
+//LICENSING
+// Copyright(C) 2021, 2025  TG Team,Key Laboratory of Jiangsu province High-Tech design of wind turbine,WTG,WL,赵子祯
+//
+//    This file is part of OpenWECD.Openhast
+//
+// Licensed under the Boost Software License - Version 1.0 - August 17th, 2003
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.HawtC.cn/licenses.txt
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT
+// SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE
+// FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+//
+//**********************************************************************************************************************************
+
+using System.Numerics;
+using MathNet.Numerics.LinearAlgebra;
+using MathNet.Numerics.LinearAlgebra.Complex;
+using OpenWECD.PreComp;
+using NumpyDotNet;
+using OpenWECD.IO.IO;
+using OpenWECD.IO.Log;
+using OpenWECD.IO.Log;
+
+namespace OpenWECD.HawtC2
+{
+    /// <summary>
+    /// 这个版本的主要目的是
+    /// </summary>
+    public class PreComp_Pro
+    {
+        static void Main(string[] args)
+        {
+            np.tuning.EnableTryCatchOnCalculations = false;
+            LogHelper.DisplayInformation(false,false);
+            INI_IO.INIProgram();
+
+            string path = "";
+            //# 31m叶片测试案例
+            path = @"G:\2026\HawtC2\demo\PreComp\test01_composite_blade.pci";
+
+
+            //TimesHelper.Tic();
+            //var a2 = Matrix<double>.Build.Random(40, 40);
+            //var b2 = a2 + a2;
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    b2 = b2 * b2 + b2;
+            //}
+            //Console.WriteLine(b2.ToString());
+            //TimesHelper.Toc();
+
+            //var test = np.array(b2);
+            //Console.WriteLine(test.shape);
+
+
+            //TimesHelper.Tic();
+            //var rand = new np.random();
+            //rand.seed(100);
+            //var a = rand.rand(new shape(10000, 3, 40, 40));
+            //var b =(ndarray)a[1, 1, ":", ":"] ;
+            //for (int i = 0; i < 100000; i++)
+            //{
+            //    b = (ndarray)a[1, 1, ":", ":"];
+            //    b = b * b + b;
+            //}
+
+            //Console.WriteLine(b.ToString());
+            //TimesHelper.Toc();
+
+            //TimesHelper.Tic();
+            //var a1 = new Vector3(0.1f, 0.2f, 0.3f);
+            //var b1 = a1 + a1;
+            //for (int i = 0; i < 1000000; i++)
+            //{
+            //    b1 = Vector3.Dot(a1, b1) * b1;
+            //}
+            //Console.WriteLine(b1.ToString());
+            //TimesHelper.Toc();
+
+
+
+#if RELEASE
+            //# 不经过CLi系统只能允许文件
+
+            if (args.Length == 1)
+            {
+                path = args[0];
+                goto Label;
+            }
+            else if (args.Contains("cli") | args.Contains("CLI"))
+            {
+                path = Path.Combine(path.GetDirectoryName(), path.GetFileNameWithoutExtension()) + ".yml";
+                var pci = OpenWECD.PreComp.PciL_IO_Subs.ReadPreCompL_MainFile(args[1]);
+                path = path + "pci";
+                var yaml13 = new OpenWECD.IO.IO.YML(path);
+                new OpenWECD.PreComp.PciL_IO_Subs().ConvertToYML(ref yaml13, pci);
+                yaml13.save();
+                LogHelper.EndProgram(keepstay: true);
+            }
+            Console.Write(" > ");
+            path = Console.ReadLine();
+            if (path is null)
+            {
+                LogHelper.ErrorLog("The file path is null!", FunctionName: "Main");
+                path = " ";
+            }
+#endif
+#if DEBUG
+
+
+#endif
+        Label:
+            CheckError.Filexists(path);
+            var temp1 = File.ReadAllLines(path);
+            string inf = temp1[0];
+            LogHelper.WriteLog("------------------------------------------------------------------------------------", color: ConsoleColor.Blue, show_title: false, leval: 1);
+            LogHelper.WriteLog("! The PreComp Analysis:" + temp1[1], color: ConsoleColor.Blue, show_title: false, leval: 1);
+            LogHelper.WriteLog("------------------------------------------------------------------------------------", color: ConsoleColor.Blue, show_title: false, leval: 1);
+
+
+            //# 先读取文件
+            var pre = OpenWECD.PreComp.PciL_IO_Subs.ReadPreCompL_MainFile(path);
+            var pe = new PciL_Subs(pre);
+            pe.run();
+            LogHelper.EndProgram(keepstay: true);
+            Console.WriteLine();
+
+
+        }
+
+    }
+}
